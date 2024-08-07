389a7a051ad34822b9e6f24a57b245de19a2ecfd
Todd Littlejohn <toddlittlejohn@Todds-MacBook-Pro-2.local>
2024-08-06 21:28:32 -0600

    we can now copy a blink

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -2,7 +2,6 @@
 import StreamlabsAuthButton from "@/components/StreamlabsAuthButton";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { useEffect, useState } from "react";
  
 export default function Address() {
@@ -11,13 +10,26 @@ export default function Address() {
   const [balance, setBalance] = useState<number>(0);
   // code for the `getBalanceEvery10Seconds` and useEffect code here
  
+  function copyBlink() {
+    const textToCopy = `http://localhost:3000/donate?to=${publicKey!}&message={message}`;
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        alert("Text copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
       {publicKey ? (
         <div className="flex flex-col gap-4">
-          <button className="btn btn-primary">Button</button>
           <div>
             <StreamlabsAuthButton pubkey={`${publicKey}`}  />
+          </div>
+          <div>
+            <button className="btn btn-primary" onClick={copyBlink}>Copy Blink</button>
           </div>
         </div>
       ) : (
