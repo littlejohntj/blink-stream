fe256f89c401fea4f3931f30a8921fb9af4201b0
TJ Littlejohn <tjl13c@my.fsu.edu>
2024-08-07 16:27:06 -0600

    Tj/css (#9)
    * switch wallet button to dynamic import to avoid hydration error
    
    * tj/fixed-css
    
    ---------
    
    Co-authored-by: Todd Littlejohn <toddlittlejohn@Todds-MacBook-Pro-2.local>

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -2,9 +2,15 @@
 import StreamlabsAuthButton from "@/components/StreamlabsAuthButton";
 import { blinkUrl } from "@/utils/blink-url";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
  
+export const WalletButton = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false },
+);
+
 export default function Address() {
   const { connection } = useConnection();
   const { publicKey } = useWallet();
@@ -36,7 +42,7 @@ export default function Address() {
       ) : (
         <h1>Wallet is not connected</h1>
       )}
-        <WalletMultiButton style={{}} />
+        <WalletButton style={{}} />
     </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import AppWalletProvider from "../components/AppWalletProvider";
+import '../styles/globals.css';
  
 export default function RootLayout({
   children,
