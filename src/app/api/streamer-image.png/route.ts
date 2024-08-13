039d2e428786d11abbc40d74bc1c1c632ee9abd2
TJ Littlejohn <tjl13c@my.fsu.edu>
2024-08-13 13:14:15 -0600

    added logo and such (#16)
    Co-authored-by: Todd Littlejohn <toddlittlejohn@Todds-MacBook-Pro-2.local>

--- src/app/api/streamer-image.png/route.ts
+++ src/app/api/streamer-image.png/route.ts
@@ -1,6 +1,8 @@
 import { streamerInfo } from '@/utils/backend/streamer-info';
 import { NextResponse } from 'next/server';
 import sharp from 'sharp';
+import fs from 'fs';
+import path from 'path';
 
 export async function GET(request: Request) {
 
@@ -9,11 +11,26 @@ export async function GET(request: Request) {
 
     const streamer = await streamerInfo(pubkey)
 
-    const createTextImage = (text: string, width: number, height: number, fontSize: number, color: string) => {
+    const loadFont = (fontPath: string) => {
+        const font = fs.readFileSync(fontPath);
+        return font.toString('base64');
+    };
+
+
+    const createTextImage = (text: string, width: number, height: number, fontSize: number, color: string, fontData: string) => {
         const svgImage = `
-            <svg width="${width}" height="${height}">
+            <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+                <style>
+                    @font-face {
+                        font-family: 'Cooper Hewitt';
+                        src: url(data:font/ttf;base64,${fontData}) format('truetype');
+                    }
+                    text {
+                        font-family: 'Cooper Hewitt';
+                    }
+                </style>
                 <rect width="100%" height="100%" fill="transparent"/>
-                <text x="50%" y="50%" font-family="Arial" font-size="${fontSize}" fill="${color}" text-anchor="middle" alignment-baseline="middle">${text}</text>
+                <text x="50%" y="50%" font-family="Cooper Hewitt" font-size="${fontSize}" fill="${color}" text-anchor="middle" alignment-baseline="middle">${text}</text>
             </svg>
         `;
         return Buffer.from(svgImage);
@@ -28,7 +45,10 @@ export async function GET(request: Request) {
         return Buffer.from(svgImage);
     };
 
-    const textImage = createTextImage(`donate to ${streamer!.name.toLowerCase()}`, 2000, 2000, 128, "white")
+    const fontPath = path.resolve('./public/fonts/cooper-hewitt-fixed-for-windows-master/CooperHewitt-Heavy.ttf');
+    const fontData = loadFont(fontPath);
+
+    const textImage = createTextImage(`donate to ${streamer!.name.toLowerCase()}`, 2000, 2000, 128, "white", fontData)
     const colorImage = createSolidColorImage("blue", 2000, 2000)
 
     const processedImage = await sharp(colorImage).composite([{input: textImage}]) .png().toBuffer();
