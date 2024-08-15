dc7f36d2a557063500a110644356976b1294f36e
Todd Littlejohn <toddlittlejohn@Todds-MacBook-Pro-2.local>
2024-08-15 00:38:50 -0600

    lets see!!!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import AppWalletProvider from "../components/AppWalletProvider";
 import '../styles/globals.css';
- 
+import Head from 'next/head';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,6 +9,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Blurt.gg</title>
+        <meta property="og:title" content="Blurt.gg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.blurt.gg" />
+        <meta property="og:image" content="https://www.blurt.gg/blurt.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="blurt.gg" />
+        <meta property="twitter:url" content="https://www.blurt.gg" />
+        <meta name="twitter:title" content="Blurt.gg" />
+        <meta name="twitter:description" content="Donate to your favorite streamers." />
+        <meta name="twitter:image" content="https://www.blurt.gg/blurt.png" />
+      </Head>
       <body>
         <AppWalletProvider>{children}</AppWalletProvider>
       </body>
