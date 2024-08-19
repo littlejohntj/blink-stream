be4e22e6218da97f3884e1cfe01a46b5e4eb6198
TJ Littlejohn <tjl13c@my.fsu.edu>
2024-08-19 11:34:29 -0600

    Tj/blink gif (#18)
    * lets see!!!
    
    * again!
    
    ---------
    
    Co-authored-by: Todd Littlejohn <toddlittlejohn@Todds-MacBook-Pro-2.local>

--- src/app/api/helius/route.ts
+++ src/app/api/helius/route.ts
@@ -3,6 +3,10 @@ import * as bs58 from 'bs58';
 import axios from 'axios';
 import prisma from '../../../utils/backend/prisma'
 import { displayStringForTokenString } from '@/utils/shared/supported-tokens';
+import OpenAI from 'openai';
+import fs from 'fs';
+import path from 'path';
+import { writeFile } from "fs/promises";
 
 export async function POST(request: Request) {
 
@@ -38,14 +42,37 @@ export async function POST(request: Request) {
         }
     })
 
-    console.log(streamerPubkey)
-    console.log(streamer)
+    const client = new OpenAI({
+        apiKey: process.env['OPENAI_API_KEY']!, // This is the default and can be omitted
+    });
+
+    const response = await client.audio.speech.create({
+        "model": "tts-1",
+        "voice": "alloy",
+        "input": tipDescription.message,
+    })
+
+    const buffer = Buffer.from( await response.arrayBuffer() )    
+        // // @ts-ignore
+        const filename = `${Date.now()}.mp3`
+    
+        // console.log(filename)
+    
+        try {
+            await writeFile(
+              path.join(process.cwd(), "public/sounds/" + filename),
+              buffer
+            );
+        } catch (error) {
+            console.log("Error occured ", error);
+            return NextResponse.json({ Message: "Failed", status: 500 });
+        }
 
 
     const reponse = await axios.post("https://streamlabs.com/api/v2.0/alerts", {
         "type": "donation",
         "image_href": "",
-        "sound_href": "",
+        "sound_href": `https://www.blurt.gg/sounds/${filename}`,
         "message": tipDescription.message,
         "user_message": `${tipDescription.name} donated ${tipDescription.amount} ${ displayStringForTokenString(tipDescription.token) }`,
         "duration": "8000",
---
+++ src/app/api/voice/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+import fs from 'fs';
+import path from 'path';
+import { writeFile } from "fs/promises";
+
+
+
+export async function GET(request: Request) {
+
+
+
+    return NextResponse.json({});
+}
+
