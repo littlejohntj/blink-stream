d76e2e5258bd474dc9f4fe6abfe021648949c842
Todd Littlejohn <toddlittlejohn@Todds-MacBook-Pro-2.local>
2024-08-20 14:42:07 -0600

    lfg

--- src/utils/shared/blink-url.ts
+++ src/utils/shared/blink-url.ts
@@ -1,4 +1,4 @@
 export const blinkUrl = ( toPubkey: string ): string => {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-    return `${baseUrl}/donate?to=${toPubkey}&message={message}&amount={amount}&name={name}&token={token}`
+    return `${baseUrl}/donate?to=${toPubkey}`
 }
